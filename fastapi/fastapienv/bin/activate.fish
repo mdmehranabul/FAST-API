a1e0dad7618b5851096b8bf68edeab21ee74ab13
mdmehranabul <mehranabul@gmail.com>
2025-10-25 00:20:03 +0300

    FAST API request added

--- fastapi/fastapienv/bin/activate.fish
+++ fastapi/fastapienv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/mdmehranabul/Documents/fastapi/fastapienv"
+set -gx VIRTUAL_ENV "/Users/mdmehranabul/PycharmProjects/FAST-API/fastapi/fastapienv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
