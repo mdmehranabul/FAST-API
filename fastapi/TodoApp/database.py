08c3b031ad111d0b11e4b9725b072a9bdecf425a
mdmehranabul <mehranabul@gmail.com>
2025-11-05 20:12:45 +0300

    Added Authentication and Authorization

--- fastapi/TodoApp/database.py
+++ fastapi/TodoApp/database.py
@@ -3,10 +3,10 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todos.db'
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
-session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
