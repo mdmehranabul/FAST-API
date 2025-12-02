43dfc6b7e5ce562e5e05843c170a2b5f40d692ee
mdmehranabul <mehranabul@gmail.com>
2025-12-03 00:00:35 +0530

    adding multiplication function

--- python-git-basics/MathFunctions.py
+++ python-git-basics/MathFunctions.py
@@ -4,5 +4,9 @@ def additions(a,b):
 def subtractions(a,b):
     return a-b
 
+def multiplications(a,b):
+    return a*b
+
 print(additions(10,5))
 print(subtractions(10,5))
+print(multiplications(10,5))
